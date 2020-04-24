02d7a8bc688f8855e2dca04933a0a29bb1aa4724
Akshay Iyer <akshay.rajagopal@gmail.com>
2020-04-24 16:20:19 -0400

    Code refactoring

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react"
+import React from "react"
 
-class Contact extends Component {
-    render() {
-        return (
-            <section className="panel color4-alt">
-                <div className="inner columns divided">
-                    <div className="span-1-6">
-                        <ul className="contact-icons color1">
-                            <li className="icon brands fa-linkedin">
-                                <a href="https://www.linkedin.com/in/akshayiyer/" target="_blank" rel="noopener noreferrer">linkedin.com/in/aiyer16</a>
-                            </li>
-                            <li className="icon brands fa-github">
-                                <a href="https://github.com/aiyer16" target="_blank" rel="noopener noreferrer">aiyer16</a>
-                            </li>
-                            <li className="icon brands fa-facebook-f">
-                                <a href="https://www.facebook.com/akshay.iyer.5" target="_blank" rel="noopener noreferrer">facebook.com/akshay.iyer.5</a>
-                            </li>
-                            <li className="icon brands fa-instagram">
-                                <a href="https://www.instagram.com/akshayiyer16/" target="_blank" rel="noopener noreferrer">@akshayiyer16</a>
-                            </li>
-                        </ul>
-                    </div>
+function Contact() {
+    return (
+        <section className="panel color4-alt">
+            <div className="inner columns divided">
+                <div className="span-1-6">
+                    <ul className="contact-icons color1">
+                        <li className="icon brands fa-linkedin">
+                            <a href="https://www.linkedin.com/in/akshayiyer/" target="_blank" rel="noopener noreferrer">linkedin.com/in/aiyer16</a>
+                        </li>
+                        <li className="icon brands fa-github">
+                            <a href="https://github.com/aiyer16" target="_blank" rel="noopener noreferrer">aiyer16</a>
+                        </li>
+                        <li className="icon brands fa-facebook-f">
+                            <a href="https://www.facebook.com/akshay.iyer.5" target="_blank" rel="noopener noreferrer">facebook.com/akshay.iyer.5</a>
+                        </li>
+                        <li className="icon brands fa-instagram">
+                            <a href="https://www.instagram.com/akshayiyer16/" target="_blank" rel="noopener noreferrer">@akshayiyer16</a>
+                        </li>
+                    </ul>
                 </div>
-            </section>
-        )
-    }
+            </div>
+        </section>
+    )
 }
 
 export default Contact
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,25 @@
-import React, { Component } from "react"
+import React from "react"
 
-class Home extends Component {
-    render() {
-        return (
-            /* Panel (Banner) */
-            < section className="panel banner right" >
-                <div className="content color0 span-3-75">
-                    <h1 className="major">Akshay Iyer</h1>
-                    <p style={{
-                        fontWeight: "bold",
-                        WebkitAnimation: "focus-in-expand 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
-                        animation: "focus-in-expand 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
-                    }}>DATA ENGINEER . MUSICIAN . PHOTOGRAPHER</p>
-                    <ul className="actions">
-                        <li><a href="#first" className="button primary color1 circle icon solid fa-angle-right">Next</a></li>
-                    </ul>
-                </div>
-                <div className="image filtered span-1-75" data-position="25% 25%">
-                    <img src="images/profile.jpg" alt="" />
-                </div>
-            </section >
-        )
-    }
+function Home() {
+    return (
+        /* Panel (Banner) */
+        < section className="panel banner right" >
+            <div className="content color0 span-3-75">
+                <h1 className="major">Akshay Iyer</h1>
+                <p style={{
+                    fontWeight: "bold",
+                    WebkitAnimation: "focus-in-expand 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+                    animation: "focus-in-expand 2s cubic-bezier(0.250, 0.460, 0.450, 0.940) both"
+                }}>DATA ENGINEER . MUSICIAN . PHOTOGRAPHER</p>
+                <ul className="actions">
+                    <li><a href="#first" className="button primary color1 circle icon solid fa-angle-right">Next</a></li>
+                </ul>
+            </div>
+            <div className="image filtered span-1-75" data-position="25% 25%">
+                <img src="images/profile.jpg" alt="" />
+            </div>
+        </section >
+    )
 }
 
 export default Home
